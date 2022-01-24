778cfeeda247ecbf380547938164fa9a19d87266
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-01-24 17:32:03 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        System.out.println(curso);
+
+    }
+}
